70992e691341f474bfaed6ee7360c86ad1de8d0c
Federico Barbieri <federicobarbieri54@gmail.com>
2024-11-09 21:38:50 +0100

    add sign in function

--- backend/src/handlers/user.ts
+++ backend/src/handlers/user.ts
@@ -1,5 +1,5 @@
 import prisma from "../db"
-import {createJWT, hashPassword} from "../modules/auth"
+import {createJWT, hashPassword, comparePasswords} from "../modules/auth"
 
 
 export const createNewUser = async (req: request, res: response) => {
@@ -13,4 +13,26 @@ export const createNewUser = async (req: request, res: response) => {
     })
     const token = createJWT(user)
     res.json({token: token})
+}
+
+
+
+export const signIn = (req: request, res: response) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            username: req.body.username
+        }
+    })
+
+    const isValid = await comparePasswords(req.body.password, user.password)
+
+    if(!valid){
+        res.status(401)
+        res.json({message: 'nej tak'})
+        return
+    }
+
+    const token = createJWT(user)
+    res.json({token: token})
+
 }
