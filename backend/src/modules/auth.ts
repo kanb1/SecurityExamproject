5492a1dd6d03cf6c6f60a85ec83e05e25b52b9a7
Federico Barbieri <federicobarbieri54@gmail.com>
2024-11-09 21:08:22 +0100

    verify json  web token

--- backend/src/modules/auth.ts
+++ backend/src/modules/auth.ts
@@ -15,7 +15,7 @@ return token
 }
 
 
-export const protect = (req: request, res: response) => {
+export const protect = (req: request, res: response, next) => {
     const bearer = req.headers.authorization;
 
     if(!bearer){
@@ -23,4 +23,24 @@ export const protect = (req: request, res: response) => {
         res.json({message: 'not authorized'});
         return
     }
+
+    const [, token] = bearer.split(' ')
+
+    if (!token){
+        res.status(401);
+        res.json({message: 'not valid token'});
+    }
+
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = user
+            next()
+    } catch (e) {
+        console.error(e);
+        res.status(401);
+        res.json({message: 'not valid token'});
+        return
+    }
+
+
 }
